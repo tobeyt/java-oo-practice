43f62c997cdb7bc23de86b69f74a5dc594d31768
Tobey <bitsqiu@gmail.com>
2020-07-19 17:21:54 +0800

    fix: main menu

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,7 +1,39 @@
 package com.twu;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        while (true) {
+            Menu.mainMenu();
+            int c = Integer.parseInt(getInput());
+            try {
+                switch (c) {
+                    // 用户
+                    case 1:
+                        System.out.println("1");
+                        break;
+                    // 管理员
+                    case 2:
+                        System.out.println("2");
+                        break;
+                    // 退出
+                    case 3:
+                        System.exit(0);
+                    default:
+                        System.out.println("输入有误");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static Scanner sc = null;
+
+    public static String getInput() {
+        sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
---
+++ src/com/twu/Menu.java
@@ -0,0 +1,25 @@
+package com.twu;
+
+public class Menu {
+    public static void mainMenu(){
+        System.out.println("欢迎来到热搜排行榜，你是？");
+        System.out.println("1.用户");
+        System.out.println("2.管理员");
+        System.out.println("3.退出");
+    }
+
+    public static void adminMenu(){
+        System.out.println("1.查看热搜排行榜");
+        System.out.println("2.添加热搜");
+        System.out.println("3.添加超级热搜");
+        System.out.println("4.退出");
+    }
+
+    public static void userMenu(){
+        System.out.println("1.查看热搜排行榜");
+        System.out.println("2.给热搜事件投票");
+        System.out.println("3.购买热搜");
+        System.out.println("4.添加热搜");
+        System.out.println("5.退出");
+    }
+}
